fbb84b9a4733822cab8f814141ba649d3e7ebbd9
Przemyslaw Piatek <przemyslaw.piatek@pl.abb.com>
2018-02-22 15:20:00 +0100

    New quarternotes photos added

--- MusicNotes/renaming_images.py
+++ MusicNotes/renaming_images.py
@@ -10,12 +10,14 @@ directory_path = os.path.join(os.path.dirname(sys.argv[0]), directory_name)
 
 print(directory_path)
 
+filenames = os.listdir(directory_path)
+
 i = 1
 
 for filename in os.listdir(directory_path):
     extension = filename.split('.')[-1]
     old_filename_path = os.path.join(directory_path, filename)
-    new_filename_path = os.path.join(directory_path, directory_name + str(i) + '.' + extension)
+    new_filename_path = os.path.join(directory_path, directory_name + '_' + str(i) + '.' + extension)
     print('Renaming', old_filename_path, 'to', new_filename_path)
     os.rename(old_filename_path, new_filename_path)
     i += 1
