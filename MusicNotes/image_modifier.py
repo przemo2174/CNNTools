575f124f6218b9cde0d26553d434c26f1d029899
Przemyslaw Piatek <przemyslaw.piatek@pl.abb.com>
2018-03-02 14:08:24 +0100

    PNG change

--- MusicNotes/image_modifier.py
+++ MusicNotes/image_modifier.py
@@ -5,13 +5,13 @@ import os
 
 class ImageModifier:
     def __init__(self, images_paths):
-        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+        sometimes = lambda aug: iaa.Sometimes(0.9, aug)
         self.seq = iaa.Sequential([       
             sometimes(iaa.Affine(
-                scale={"x": (0.6, 1.5), "y": (0.6, 1.5)},
-                rotate=(-45, 45)
-            ))
-            # iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
+                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                rotate=(-15, 15)           
+            )),
+            iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
             ])
         self.images_paths = images_paths
         self.modified_images = []        
@@ -27,5 +27,5 @@ class ImageModifier:
             self.modified_images = self.seq.augment_images(images_data)
             
             for modify_image in self.modified_images:
-                misc.imsave(os.path.join(output_folder, 'image_' + str(counter) + '.jpg'), modify_image)
+                misc.imsave(os.path.join(output_folder, 'image_' + str(counter) + '.png'), modify_image)
                 counter += 1
